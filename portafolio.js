fad7294ac95eeaf28ab2e4b2aaa82f9fff4547a0
Felipe Cortez <hola@faco.cl>
2019-08-06 20:12:35 -0400

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -2,37 +2,44 @@
 var trabajos = [
 {
     figure: "media/foto-01.jpg",
-    title: "Lorem ipsum 01",
-    about: "Nulla pretium non sapien eget fermentum",
+    alt:"Descripción de la imagen",
+    title: "Título de primer trabajo",
+    about: "Texto simulado de descripción de primer trabajo"
 }, {
     figure: "media/foto-02.jpg",
-    title: "Lorem ipsum 02",
-    about: "Nulla pretium non sapien eget fermentum",
+    alt:"Descripción de la imagen",
+    title: "Título de segundo trabajo",
+    about: "Texto simulado de descripción de segundo trabajo"
 }, {
     figure: "media/foto-03.jpg",
-    title: "Lorem ipsum 03",
-    about: "Nulla pretium non sapien eget fermentum",
+    alt:"Descripción de la imagen",    
+    title: "Título de tercer trabajo",
+    about: "Texto simulado de descripción de tercer trabajo"
 }, {
     figure: "media/foto-04.jpg",
-    title: "Lorem ipsum 04",
-    about: "Nulla pretium non sapien eget fermentum",
+    alt:"Descripción de la imagen",
+    title: "Título de cuarto trabajo",
+    about: "Texto simulado de descripción de cuarto trabajo"
 }, {
     figure: "media/foto-05.jpg",
-    title: "Lorem ipsum 05",
-    about: "Nulla pretium non sapien eget fermentum",
+    alt:"Descripción de la imagen",
+    title: "Título de quinto trabajo",
+    about: "Texto simulado de descripción de quinto trabajo"
 }, {
     figure: "media/foto-06.jpg",
-    title: "Lorem ipsum 06",
-    about: "Nulla pretium non sapien eget fermentum",
+    alt:"Descripción de la imagen",
+    title: "Título de sexto trabajo",
+    about: "Texto simulado de descripción de sexto trabajo"
 }, {
     figure: "media/foto-07.jpg",
-    title: "Lorem ipsum 07",
-    about: "Nulla pretium non sapien eget fermentum",
+    alt:"Descripción de la imagen",
+    title: "Título de séptimo trabajo",
+    about: "Texto simulado de descripción de séptimo trabajo"
 }
 ]
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
-    var uno = '<figure><img src="' + trabajos[i].figure + '"/></figure>';
+    var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
     $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
