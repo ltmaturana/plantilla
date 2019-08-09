4425c199e7972e853f2bc34466af8cc28f260d51
Felipe Cortez <hola@faco.cl>
2019-08-09 15:25:19 -0400

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -1,37 +1,37 @@
 // PRIMERO CREAMOS UNA VARIABLE QUE CONTIENE TODOS LOS DATOS DE TUS TRABAJOS
 var trabajos = [
 {
-    figure: "media/foto-01.jpg",
+    figure: "media/trabajo-01-desfile.jpg",
     alt:"Descripción de la imagen",
     title: "Título de primer trabajo",
     about: "Texto simulado de descripción de primer trabajo"
 }, {
-    figure: "media/foto-02.jpg",
+    figure: "media/trabajo-02-app.jpg",
     alt:"Descripción de la imagen",
     title: "Título de segundo trabajo",
     about: "Texto simulado de descripción de segundo trabajo"
 }, {
-    figure: "media/foto-03.jpg",
+    figure: "media/trabajo-03-homy.jpg",
     alt:"Descripción de la imagen",    
     title: "Título de tercer trabajo",
     about: "Texto simulado de descripción de tercer trabajo"
 }, {
-    figure: "media/foto-04.jpg",
+    figure: "media/trabajo-04-editorial.jpg",
     alt:"Descripción de la imagen",
     title: "Título de cuarto trabajo",
     about: "Texto simulado de descripción de cuarto trabajo"
 }, {
-    figure: "media/foto-05.jpg",
+    figure: "media/trabajo-05-artesania.jpg",
     alt:"Descripción de la imagen",
     title: "Título de quinto trabajo",
     about: "Texto simulado de descripción de quinto trabajo"
 }, {
-    figure: "media/foto-06.jpg",
+    figure: "media/trabajo-06-textil.jpg",
     alt:"Descripción de la imagen",
     title: "Título de sexto trabajo",
     about: "Texto simulado de descripción de sexto trabajo"
 }, {
-    figure: "media/foto-07.jpg",
+    figure: "media/trabajo-07-materiales.jpg",
     alt:"Descripción de la imagen",
     title: "Título de séptimo trabajo",
     about: "Texto simulado de descripción de séptimo trabajo"
